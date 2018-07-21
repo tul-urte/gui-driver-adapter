75b1a81102a61773d1ebba404056b4ad6f66b529
tul-urte <tul_urte@brentcroft.com>
2018-07-21 17:19:08 +0100

    Commit rename update

--- src/main/java/com/brentcroft/gtd/adapter/model/swing/AbstractButtonGuiObject.java
+++ src/main/java/com/brentcroft/gtd/adapter/model/swing/AbstractButtonGuiObject.java
@@ -4,7 +4,7 @@ import com.brentcroft.gtd.adapter.model.AttrSpec;
 import com.brentcroft.gtd.adapter.model.GuiObject;
 import com.brentcroft.gtd.adapter.model.GuiObjectConsultant;
 import com.brentcroft.gtd.adapter.utils.SwingUtils;
-import com.brentcroft.gtd.camera.GuiCameraObjectManager;
+import com.brentcroft.gtd.camera.CameraObjectManager;
 import com.brentcroft.util.xpath.gob.Gob;
 import java.util.Arrays;
 import java.util.List;
@@ -20,7 +20,7 @@ import static java.util.Optional.ofNullable;
  */
 public class AbstractButtonGuiObject< T extends AbstractButton > extends JComponentGuiObject< T > implements GuiObject.Click
 {
-    public AbstractButtonGuiObject( T go, Gob parent, GuiObjectConsultant< T > guiObjectConsultant, GuiCameraObjectManager objectManager )
+    public AbstractButtonGuiObject( T go, Gob parent, GuiObjectConsultant< T > guiObjectConsultant, CameraObjectManager objectManager )
     {
         super( go, parent, guiObjectConsultant, objectManager );
     }
